c5948f89d43c5d5c31722b116c2e297cdce5f6ec
Felipe de Siqueira Zanella <freeman.zanella@gmail.com>
2018-08-24 00:51:17 -0300

    funcao div

--- calc.js
+++ calc.js
@@ -6,5 +6,10 @@ sub = (a,b) => {
     return a - b;
 }
 
+div = (a,b) => {
+    return b !== 0 ? a / b : 'Divisao por 0';
+}
+
 console.log(soma(2,2));
 console.log(sub(5,2));
+console.log(div(10,0));
